9b0a856988181c866602d32a2b83af8490162f58
COQUARD Cyrille <cyrille@wp-media.me>
2022-11-05 15:55:44 +0100

    added logos

--- src/Linepay.php
+++ src/Linepay.php
@@ -8,6 +8,19 @@ use Coquardcyr\Linepay\ObjectValue\LogoType;
 class Linepay
 {
     public static function getLogo(CountryCode $code, LogoType $type, int $width) {
+        $filename = '';
+        // Get new sizes
+        list($image_width, $image_height) = getimagesize($filename);
 
+        $newwidth = $width * $percent;
+        $newheight = $height * $percent;
+        $thumb = imagecreatetruecolor($newwidth, $newheight);
+        $source = imagecreatefromjpeg($filename);
+        imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+        ob_start();
+        imagepng($thumb);
+        $image_data = ob_get_contents();
+        ob_end_clean();
+        return $image_data;
     }
 }
